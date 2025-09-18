5f675bbfa42192d526fc79e0b737a50443c9857b
webd1989 <webddh1989@gmail.com>
2025-09-18 14:15:27 +0530

    up

---
+++ public/admin/js/cabaa/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+   
+    $('.car-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        items:1,
+        center: true,
+        nav: true,
+        dots:false,
+        autoplay: false,
+        animateOut: 'slideOutDown',
+        animateIn: 'flipInX',
+    });
+   
+});
+
+  
+
+
